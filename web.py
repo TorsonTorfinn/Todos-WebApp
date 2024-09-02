b2ad5ca6c6e341153ebc1d15cf94b3a6492acf3a
Сухроб Юсубахмедов <Suhrob.Yusubaxmedov@ucell.uz>
2024-09-02 17:14:29 +0500

    Implement the complete for todo app.

--- web.py
+++ web.py
@@ -13,10 +13,15 @@ st.title("Welcome, to the Todo App!")
 st.subheader("This app was created,")
 st.write("for increase your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun() # this is needed for checkboxes
 
-st.text_input(label="", placeholder="Add new todo...",
+st.text_input(label="Please, enter a todo:", placeholder="Add new todo...",
               on_change=add_todo, key="new todo")
 
-st.session_state
+# st.session_state 
